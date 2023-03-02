4efeda269ebe82648272d2a001ad016da5174695
Patrick Rachford <prachford@icloud.com>
2023-03-02 14:44:27 -0800

    first commit

---
+++ run_worker.py
@@ -0,0 +1,20 @@
+import asyncio
+from temporalio.client import Client
+from temporalio.worker import Worker
+from your_activities import your_activity
+from your_workflows import YourWorkflow
+
+
+async def main():
+    client = await Client.connect("localhost:7233")
+    worker = Worker(
+        client,
+        task_queue="my-task-queue",
+        workflows=[YourWorkflow],
+        activities=[your_activity],
+    )
+    await worker.run()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ run_workflow.py
@@ -0,0 +1,18 @@
+import asyncio
+from temporalio.client import Client
+
+from your_workflows import YourWorkflow
+
+
+async def main():
+    client = await Client.connect("localhost:7233")
+
+    result = await client.execute_workflow(
+        YourWorkflow.run, "my name", id="my-workflow-id", task_queue="my-task-queue"
+    )
+
+    print(f"Result: {result}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
